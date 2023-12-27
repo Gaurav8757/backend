316185af94220d5b0115394d06490afb57a29b55
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2023-12-27 13:50:23 +0530

    update backend

---
+++ controller/user_controller/usercomplaint.controller.js
@@ -0,0 +1,46 @@
+import UserComplaint from "../../models/user_models/usercomplaintSchema.js";
+
+export const userComplaint = async (req, res) => {
+    try {
+      const {
+        complaint_name,
+        complaint_email,
+        complaint_mobile,
+        complaint_subject,
+        complaint_query,
+       
+      } = req.body;
+  
+      // Check if the branch with the given branchcode already exists
+      const emailExist = await UserComplaint.findOne({complaint_email});
+      if (emailExist) {
+        return res.status(400).json({
+          status: "User Already Exists",
+          message: "This user already exists.",
+        });
+      }
+//   console.log(emailExist);
+      // Create a new branch
+      const newComplaint = new UserClaim({
+        complaint_name,
+        complaint_email,
+        complaint_mobile,
+        complaint_subject,
+        complaint_query,
+      });
+      // Save the new branch to the database
+      await newComplaint.save();
+      return res.status(201).json({
+        status: "Complaint Submitted Successfully!",
+        message: {
+            newComplaint
+        },
+      });
+    } catch (err) {
+      return res.status(400).json({
+        status: "Error during Complaint..!",
+        message: err.message,
+      });
+    }
+  };
+
--- routes/user_routes/usersRoutes.js
+++ routes/user_routes/usersRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import {claimAdded} from "../../controller/user_controller/userclaim.controller.js";
+import { userComplaint } from "../../controller/user_controller/usercomplaint.controller.js";
 const router = express.Router();
 
 
 
 // add claim
 router.post("/claim", claimAdded);
+// add complaint
+router.post("/complaint", userComplaint);
 
 
 
