979d19ebae037affc4c471329f7692c74f66f0c5
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2024-01-03 19:03:42 +0530

    udates

--- controller/addcompany.controller.js
+++ controller/addcompany.controller.js
@@ -1,10 +1,5 @@
 import AddCompanies from "../models/addcompanySchema.js";
-import dotenv from "dotenv";
-
-dotenv.config();
-const {BASE} = process.env;
 // adding to db
-
 export const addCompany = async (req, res) => {
     try {
       const { comp_insurance, comp_categories, comp_establishment, comp_cname } = req.body;
@@ -12,9 +7,9 @@ export const addCompany = async (req, res) => {
       // Check if a file is provided in the request
       const comp_cfiles =
         req.files && req.files["comp_cfiles"] && req.files["comp_cfiles"][0]
-          ? `${BASE}/src/admin/uploads/` + req.files["comp_cfiles"][0].filename
+          ? "/src/admin/uploads/" + req.files["comp_cfiles"][0].filename
           : null;
-   console.log(comp_cfiles);
+  //  console.log(comp_cfiles);
       // Create a new company instance
       const addNewCompany = new AddCompanies({
         comp_insurance,
