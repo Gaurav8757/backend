896d7aed734a5d6419667b894f197f5613812b9c
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2024-01-05 13:14:36 +0530

    router - router1

--- routes/usercarousel_routes/userCarousel_routes.js
+++ routes/usercarousel_routes/userCarousel_routes.js
@@ -12,6 +12,6 @@ router1.post("/carousel", uploadFile ,firstUserCarousel);
 // view carousel lists
 router1.get("/view", firstUserCarouselList);
 // delete list of carousel
-router.delete("/deletecarousel/:id", deleteCarousel);
+router1.delete("/deletecarousel/:id", deleteCarousel);
 
 export default router1;
