f738513b76b7dc2c3ffd240b34efe7bb41346465
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2024-01-09 12:33:26 +0530

    usercarousel updateurl

--- controller/usercarousel_controller/usercarousel.controller.js
+++ controller/usercarousel_controller/usercarousel.controller.js
@@ -11,8 +11,9 @@ export const firstUserCarousel = async (req, res) => {
       // Check if a file is provided in the request
       const uploadfile =
         req.files["usercarousel_upload"] && req.files["usercarousel_upload"][0]
-          ? "/src/admin/uploads/" + req.files["usercarousel_upload"][0].filename
+          ?  req.protocol + '://' + req.get('host') + "/src/admin/uploads/" + req.files["usercarousel_upload"][0].filename
           : null;
+          
   
       // Check if the carousel with the given carousellink already exists
       const linkExist = await UserCarousel.findOne({ usercarousel_link });
