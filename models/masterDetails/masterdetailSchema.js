0d769d3046aadcb4edc6cf9317899061705156df
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2024-01-20 17:46:53 +0530

    updated

--- models/masterDetails/masterdetailSchema.js
+++ models/masterDetails/masterdetailSchema.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const AllInsurancePolicySchema = new mongoose.Schema(
   {
     entryDate: {
-      type: Date,
+      type: String,
     },
     company: {
       type: String,
