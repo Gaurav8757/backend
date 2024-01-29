4bb3bc4912c8ea7aa0ecaec7fe833c693ef2ea12
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2024-01-29 14:16:49 +0530

    update

--- routes/hrsalary/hrsalary.routes.js
+++ routes/hrsalary/hrsalary.routes.js
@@ -1,15 +1,11 @@
 import express from "express";
 const hrouter = express.Router();
-
 import { hrSalaryController, viewHrSalary, updateHRSalary, deleteHRSalary } from "../../controller/human_resources/salaryhr.controller.js";
-
-
 
 // add or view branch
 hrouter.post("/hr/addsalary", hrSalaryController);
 hrouter.get("/hr/viewsalary", viewHrSalary);
 hrouter.put("/hr/update/salary/:id", updateHRSalary);
 hrouter.delete("/hr/delete/salary/:id", deleteHRSalary);
-
 
 export default hrouter;
