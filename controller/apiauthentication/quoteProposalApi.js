ca42975cbf5b80af0345a2e09069a636c1af393c
Test <test.infoera2014@gmail.com>
2024-10-24 18:40:14 +0530

    added api quotes and proposal

--- controller/apiauthentication/quoteProposalApi.js
+++ controller/apiauthentication/quoteProposalApi.js
@@ -4,11 +4,34 @@ dotenv.config();
 const { TATA_AIG_4_WHEELER_QUOTE_URL, TATA_AIG_4_WHEELER_PROPOSAL_URL } =
   process.env;
 
+// const quoteApi = async (req, res) => {
+//   const authToken = req.headers.authorization;
+//   const data  = req.body;
+//   console.log(data);
+//   try {
+//     const response = await axios.post(`${TATA_AIG_4_WHEELER_QUOTE_URL}`, data, {
+//       headers: {
+//         Authorization: `${authToken}`,
+//         "Content-Type": "application/json",
+//       },
+//     });
+//     console.log(response.data);
+
+//     if (response.data.status === 200) {
+//       return res.status(200).json(response.data);
+//     } else {
+//       return res.json(response);
+//     }
+//   } catch (error) {
+//     return res
+//       .status(error.response?.status || 500)
+//       .json({ message: error.response?.data?.message });
+//   }
+// };
+
 const quoteApi = async (req, res) => {
   const authToken = req.headers.authorization;
-  const data = req.body; // Extract data from the request body
-  console.log(data);
-
+  const data = req.body;
   try {
     const response = await axios.post(`${TATA_AIG_4_WHEELER_QUOTE_URL}`, data, {
       headers: {
@@ -16,17 +39,13 @@ const quoteApi = async (req, res) => {
         "Content-Type": "application/json",
       },
     });
-
     if (response.data.status === 200) {
-      console.log(response.data);
-      return res.status(200).json(response.data);
+      return response.data;
     } else {
-      return res.json(response.data);
+      return response.data;
     }
   } catch (error) {
-    return res
-      .status(error.response?.status || 500)
-      .json({ message: error.response?.data?.message });
+    return res.json(error.response.data);
   }
 };
 
@@ -46,15 +65,12 @@ const proposalApi = async (req, res) => {
     );
 
     if (response.data.status === 200) {
-      console.log(response.data);
-      return res.status(200).json(response.data);
+      return response.data;
     } else {
-      return res.json(response.data);
+      return response.data;
     }
   } catch (error) {
-    return res
-      .status(error.response.status)
-      .json({ message: error.response.data.message });
+    return res.json(error.response?.data?.message);
   }
 };
 
